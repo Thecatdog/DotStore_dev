ba7912a19231b8b5980d49a42efb602b73e2b297
LimAh Lee <rima361@naver.com>
2018-06-21 15:56:38 +0900

    viewItemController 추가 커밋

--- dotstore_mysql/src/main/java/com/example/jpetstore/controller/ViewItemController.java
+++ dotstore_mysql/src/main/java/com/example/jpetstore/controller/ViewItemController.java
@@ -1,14 +1,22 @@
 package com.example.jpetstore.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.jpetstore.dao.mybatis.mapper.ReviewMapper;
 import com.example.jpetstore.domain.Item;
 import com.example.jpetstore.domain.Product;
+import com.example.jpetstore.domain.Review;
 import com.example.jpetstore.service.PetStoreFacade;
+import com.example.jpetstore.service.ReviewService;
 
 /**
  * @author Juergen Hoeller
@@ -19,6 +27,8 @@ import com.example.jpetstore.service.PetStoreFacade;
 public class ViewItemController { 
 
 	private PetStoreFacade petStore;
+	@Autowired ReviewMapper reviewMapper;
+	@Autowired ReviewService reviewService;
 
 	@Autowired
 	public void setPetStore(PetStoreFacade petStore) {
@@ -26,15 +36,20 @@ public class ViewItemController {
 	}
 
 	@RequestMapping("/shop/viewItem.do")
-	public String handleRequest(
-			@RequestParam("itemId") String itemId,
-			ModelMap model) throws Exception {
-
+	public String handleRequest(@RequestParam("itemId") String itemId, ModelMap model, HttpServletRequest request) throws Exception {
+		
 		Item item = this.petStore.getItem(itemId);
 		Product product = item.getProduct();
-	
+		
+		// 리뷰를 위한 코드 추가 
+		List<Review> reviewList = reviewMapper.getListByItemId(itemId);
+//		for(Review r : reviewList)
+//			System.out.println(r.toString());
+		model.put("reviewList", reviewList);
+		
 		model.put("item", item);
 		model.put("product", product);
+		
 		return "Item";
 	}
 
