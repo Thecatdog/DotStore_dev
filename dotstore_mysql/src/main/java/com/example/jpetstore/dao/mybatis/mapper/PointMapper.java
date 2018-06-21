e026c87765d42b19adec4b34d5664be5429881dd
LimAh Lee <rima361@naver.com>
2018-06-22 02:56:25 +0900

    리뷰 등록에 따른 포인트 적립 및 리뷰 등록 수정

--- dotstore_mysql/src/main/java/com/example/jpetstore/dao/mybatis/mapper/PointMapper.java
+++ dotstore_mysql/src/main/java/com/example/jpetstore/dao/mybatis/mapper/PointMapper.java
@@ -1,10 +1,11 @@
 package com.example.jpetstore.dao.mybatis.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.dao.DataAccessException;
 
 import com.example.jpetstore.domain.Point;
 
 @Mapper
 public interface PointMapper {
-	void addPoint(Point point);
+	void addPoint(Point point) throws DataAccessException;
 }
